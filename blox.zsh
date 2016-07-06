3c2a1fba323ab75081cfede4357fa131781e8e83
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-07-06 13:48:14 +0300

    forgot to build... again.

--- blox.zsh
+++ blox.zsh
@@ -243,6 +243,33 @@ function blox_block__nodejs() {
   echo $res
 }
 # --------------------------------------------- #
+# | Block options
+# --------------------------------------------- #
+BLOX_BLOCK__RUBY_SYMBOL="${BLOX_BLOCK__RUBY_SYMBOL:-♦}"
+BLOX_BLOCK__RUBY_COLOR="${BLOX_BLOCK__RUBY_COLOR:-red}"
+
+# --------------------------------------------- #
+# | The block itself
+# --------------------------------------------- #
+function blox_block__ruby() {
+
+  [[ ! -f "$(pwd)/Gemfile" ]] && return
+  local ruby_version=$(ruby --version | awk '{print $2}' | awk -F'p' '{print $1}')
+
+  # The result
+  res=""
+
+  # Build the block
+  if [[ ! -z "${ruby_version}" ]]; then
+    res+="%{$fg[${BLOX_BLOCK__RUBY_COLOR}]%}"
+    res+="${BLOX_CONF__BLOCK_PREFIX}${BLOX_BLOCK__RUBY_SYMBOL} ${node_version:1}${BLOX_CONF__BLOCK_SUFFIX}";
+    res+="%{$reset_color%}"
+  fi
+
+  # Echo the block
+  echo $res
+}
+# --------------------------------------------- #
 # | The block itself
 # --------------------------------------------- #
 function blox_block__time() {
