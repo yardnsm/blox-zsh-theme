cce01449ed9d5ac908c1af655d99e679a9e50872
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-06-15 19:54:13 +0300

    Improved the cwd block (#4)
    * Display `../` when the path is truncated.

--- src/blocks/cwd.zsh
+++ src/blocks/cwd.zsh
@@ -10,7 +10,7 @@ function blox_block__cwd() {
   local result=""
 
   result="%F{${BLOX_BLOCK__CWD_COLOR}}"
-  result+="%${BLOX_BLOCK__CWD_TRUNC}~";
+  result+="%($(( $BLOX_BLOCK__CWD_TRUNC + 1 ))~|.../%$BLOX_BLOCK__CWD_TRUNC~|%~)";
   result+="%{$reset_color%}"
 
   echo $result
